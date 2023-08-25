eb6d91d12323517d33b7dedcc306d2b5e8695248
DC1\zhuge <785930041@qq.com>
2023-08-25 11:42:38 +0800

    从另一个项目完整拷贝（带Library），首次打开生成的文件

---
+++ Tweener/Assets/Scripts/BuildGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public static class BuildGame
+{
+    public static void Build()
+    {
+        // AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.None,
+        //     BuildTarget.StandaloneWindows);
+    }
+}
